f2e2acb8ac6dd207338c524913a62731696e94b7
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-09 22:11:51 +0530

    Added: GreetingController uses GreetingService methods

--- src/main/java/com/example/greetingapp/controllers/GreetingController.java
+++ src/main/java/com/example/greetingapp/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.greetingapp.controllers;
 
+import com.example.greetingapp.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("/")
     public String greeting() {
         return "Hello! Welcome to Greeting App!";
@@ -20,6 +26,11 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "fname") String fname, @RequestParam(value = "lname") String lname) {
         return "Hello " + fname + " " + lname +"! Welcome to Greeting App!";
+    }
+
+    @GetMapping("/greeting-service")
+    public String greetingService(){
+        return greetingService.simpleHelloGreeting();
     }
 
 
